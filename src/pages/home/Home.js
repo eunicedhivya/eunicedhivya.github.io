17861641bf4eef46728a6078bd8ba9c907214554
Eunice Dhivya <divi.seed@gmail.com>
2022-07-31 21:11:32 +0530

    add home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,7 +3,46 @@ import './home.css';
 
 function Home() {
   return (
-    <div>Home</div>
+    <section className="" id="home">
+            <h1>Eunice Dhivya</h1>
+            <p>I’m a fullstack developer specializing in designing and developing dashboards and interactive stories.</p>
+            <nav className="home-nav">
+                <ul className='nav__list'>
+                    <li className="nav__item">
+                      <a href="#home" className="nav__link">
+                        <span className="icon-home hover-icon"></span>
+                        <span className="icon-text">Home</span>
+                      </a>
+                    </li>
+                    <li className="nav__item">
+                      <a href="#about" className="nav__link">
+                        <span className="icon-user hover-icon"></span>
+                        <span className="icon-text">About</span>
+                      </a>
+                    </li>
+                    <li className="nav__item">
+                      <a href="#portfolio" className="nav__link">
+                        <span className="icon-briefcase hover-icon"></span>
+                        <span className="icon-text">Portfolio</span>
+                      </a>
+                    </li>
+                    <li className="nav__item">
+                      <a href="#contact" className="nav__link">
+                        <span className="icon-envelope hover-icon"></span>
+                        <span className="icon-text">Contact</span>
+                      </a>
+                    </li>
+                  </ul>
+            </nav>
+            <div className='scroll__down'>
+                <a href="#about" className="mouse__wrapper">
+                    <span className="home__scroll-name">Scroll Down</span>
+                    <span className="mouse">
+                        <span className="wheel"></span>
+                    </span>
+                </a>
+            </div>
+        </section>
   )
 }
 
