549ae6a4c8e38b6bba14159b6a643521ff959231
Eunice Dhivya <divi.seed@gmail.com>
2022-08-01 21:47:11 +0530

    move sticky code to app.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,45 @@ import Sidebar from './components/Sidebar';
 import About from './pages/about/About';
 import Home from './pages/home/Home';
 import Portfolio from './pages/portfolio/Portfolio';
+import { useState, useEffect } from 'react';
 // 
 // import { Link } from 'react-router-dom';
 
 function App() {
+
+  const [sticky, setSticky] = useState(false);
+    
+
+    const makeSticky = () => {
+      const innerContent = document.getElementById("innerContent");
+      let innerContentPos = innerContent.offsetTop - 50;
+      // let innerContentPos = innerContent.getBoundingClientRect().top + 300;
+        // console.log(innerContentPos);
+        if(window.scrollY >= innerContentPos){
+            setSticky(true);
+        }else{
+            setSticky(false);
+        }
+    }
+
+    useEffect(() => {
+      window.addEventListener("scroll", makeSticky);
+  
+      return () => {
+        window.removeEventListener("scroll", makeSticky);
+      };
+    }, []);
   
 
   return (
     <div className="App">
       <Home />
       <div id="innerContent">
-        <Sidebar />
-        <About />
-        <Portfolio />
+        <Sidebar sticky={sticky} />
+        <main className={sticky? "site-content shift" : "site-content" }>
+          <About />
+          <Portfolio />
+        </main>
       </div>
     </div>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,23 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MainMenu from './MainMenu';
 
-function Sidebar() {
-    const innerContent = document.getElementById("innerContent");
-    // let innerContentPos = innerContent.getBoundingClientRect().top;
-    const [sticky, setSticky] = useState(false);
-    
-
-    // const makeSticky = () => {
-    //     if(window.scrollY >= innerContentPos){
-    //         setSticky(true);
-    //     }else{
-    //         setSticky(false);
-    //     }
-    // }
-
-    console.log(innerContent);
-    
-    // window.addEventListener("scroll", makeSticky);
+function Sidebar({ sticky }) {
 
   return (
     <nav className={sticky? "site-nav sticky" : "site-nav" } id="site-nav">
